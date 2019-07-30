454af84aba16b44fcf59b73bf5bd42c157ed8c26
SoujanyaSenthil <51333643+SoujanyaSenthil@users.noreply.github.com>
2019-07-30 09:22:14 -0700

    Update proqn17.py

--- proqn17.py
+++ proqn17.py
@@ -1,9 +1,9 @@
-so,sa=map(int,input().split())
+o,a=map(int,input().split())
 m=list(map(int,input().split()))
 b=0
-for i in range(0,so):
-  for j in range(m,so):
-    if l[i]+l[j]==sa and i!=j:
+for i in range(0,o):
+  for j in range(1,o):
+    if l[i]+l[j]==a and i!=j:
       b=1
       break
-print("yes" if p else "no")
+print("yes" if b else "no")
